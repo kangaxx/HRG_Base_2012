b3476dea4ac9b4151ae1e02236301bf14390942e
kangaxx <lion_kangaxx@126.com>
2018-04-08 09:40:19 +0800

    GUI draw function

--- HRG_Base_2012/HRG_DrawHelper.cs
+++ HRG_Base_2012/HRG_DrawHelper.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace HRG_Base_2012
+namespace HRG_BaseLibrary_2012
 {
     #region 绘图程序
-    class HRG_DrawHelper
+    public class HRG_DrawHelper
     {
         private Graphics _DrawRegion;
         private Color _color;
@@ -34,8 +34,33 @@ namespace HRG_Base_2012
 
         public void AnimateDemo()
         {
+            int x = 100;
+            int y = 100;
+            for (int i = 0; i <60; i++)
+            {
+                Clear();
 
+                _DrawRegion.FillEllipse(new SolidBrush( Color.Green), x, y, 50, 50);
+                System.Threading.Thread.Sleep(40);
+                x = x + 2;
+                y = y + 2;
+            }
+        }
 
+        //width 宽度  ， height 高度, time时间（秒）
+        public void EllipseMove(Point start, Point end, int width, int height, int time, SolidBrush brush)
+        {
+            int FlagNum = time * GlobalVariables.INT_HRG_FLAGNUM_PERSECOND;
+            double X_DIST = Convert.ToDouble(end.X - start.X) / Convert.ToDouble(FlagNum);
+            double Y_DIST = Convert.ToDouble(end.Y - start.Y) / Convert.ToDouble(FlagNum);
+            for(int i = 0; i < FlagNum; i++)
+            {
+                Clear();
+
+                _DrawRegion.FillEllipse(new SolidBrush(Color.Green), start.X + Convert.ToInt32(i * X_DIST), start.Y + Convert.ToInt32(i * Y_DIST) , 50, 50);
+                System.Threading.Thread.Sleep(GlobalVariables.INT_HRG_FLAGINTERVAL);
+
+            }
         }
     }
     #endregion
